df0d7a67e6fef26f4579bd0077727f9d54593808
Michał Kędrzyński <kedrzu@gmail.com>
2025-03-27 10:05:09 +0100

    chore: cleanup

--- packages/components/src/Button.lite.tsx
+++
@@ -1,7 +0,0 @@
-import { useState } from '@builder.io/mitosis';
-
-export default function Button(props: { onClick?: () => void }) {
-    const [count, setCount] = useState(0);
-
-    return <button onClick={() => setCount(count + 1)}>Click me {count}</button>;
-}
--- packages/components/src/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Button } from './Button.lite.jsx';
-export { default as PointSelector } from './PointSelector.lite.jsx';
