6cae32bdd79f37265714ddc2eb8a21f21ccfead4
Michał Kędrzyński <kedrzu@gmail.com>
2025-03-28 10:20:03 +0100

    chore: prepare for release

--- packages/common/tests/smoke.test.mjs
+++ packages/common/tests/smoke.test.mjs
@@ -1,4 +1,4 @@
-import * as paczkoapi from '@paczkoapi/client';
+import * as paczkoapi from '@paczkoapi/common';
 
 // Simple smoke test to verify the package can be imported
 if (!paczkoapi) throw new Error('Package failed to load');
