78fa17670c750c1a3337e44939d922ffca9a9c76
Michał Kędrzyński <kedrzu@gmail.com>
2025-04-01 15:59:03 +0200

    chore: remove normalizeAddress

--- packages/common/src/normalizeAddress.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Normalizes an address by removing extra spaces and converting to lowercase.
- */
-export function normalizeAddress(address: string) {
-    return address.trim().replace(/\s+/g, ' ').toLowerCase();
-}
