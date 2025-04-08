c490d6d59918344eff2ab6c7d2b2871860728460
Michał Kędrzyński <kedrzu@gmail.com>
2025-04-08 17:28:01 +0200

    chore: update tsconfigs

--- packages/web/src/components.d.ts
+++ packages/web/src/components.d.ts
@@ -5,10 +5,6 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
-import { PickupPoint, Provider } from "@paczkoapi/client";
-import { Address } from "@paczkoapi/common";
-export { PickupPoint, Provider } from "@paczkoapi/client";
-export { Address } from "@paczkoapi/common";
 export namespace Components {
     interface PaczkoapiModal {
         /**
@@ -20,7 +16,7 @@ export namespace Components {
         /**
           * The address to search for pickup points
          */
-        "address": Address | null;
+        "address": Address | null | undefined;
         /**
           * The city to search for pickup points
          */
@@ -136,7 +132,7 @@ declare namespace LocalJSX {
         /**
           * The address to search for pickup points
          */
-        "address"?: Address | null;
+        "address"?: Address | null | undefined;
         /**
           * The city to search for pickup points
          */
